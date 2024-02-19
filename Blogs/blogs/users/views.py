95fc5c7a164963432a8c96a724d1f2ca7e59e423
official-okello <officialokello@gmail.com>
2024-02-19 21:55:32 +0300

    Login page working using django templates

--- Blogs/blogs/users/views.py
+++ Blogs/blogs/users/views.py
@@ -7,9 +7,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid:
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account successfully created. Ready to log in now!  ')
+            return redirect('login')
     else:
          form = UserRegisterForm()   
     return render(request, 'users/register.html', {'form': form, 'title': 'Register'})
