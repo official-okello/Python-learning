272dbc1a6cf2dfb714e5466b6b248ecb69c362e1
official-okello <officialokello@gmail.com>
2024-07-14 11:22:26 +0300

    more py

---
+++ basics/compare .py
@@ -0,0 +1,11 @@
+x = int(input('Whats x? '))
+y = int(input('Whats y? '))
+
+if x < y:
+  print('x is less than y')
+
+elif x > y:
+  print('x is greater than y')
+
+else:
+  print('x is equal to y')
