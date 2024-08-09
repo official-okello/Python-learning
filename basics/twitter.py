ad8a7773d79b5cafc9a86dd3b0dd190a68c077a8
official-okello <officialokello@gmail.com>
2024-08-09 11:56:06 +0300

    more py

--- basics/twitter.py
+++ basics/twitter.py
@@ -5,5 +5,5 @@ url = input('URL: ').strip()
 username = re.sub(r'^(https?://)?(www\.)?twitter\.com/', '', url)
 print(f'Username: {username}')
 
-if matches := re.search(r'^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)$', url, re.IGNORECASE):
-    print(f'Username:', matches.group(1))
+if matches := re.search(r'^(https?://)?(?:www\.)?twitter\.com/([a-z0-9_]+)$', url, re.IGNORECASE):
+    print(f'Username:', matches.group(2))
